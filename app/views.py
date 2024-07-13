52b8a57ced9ee42b8ea2c4138ef2c5c700314564
SANTIAGO FIERRO <santimarcelofierrod@gmail.com>
2024-07-13 15:19:41 -0500

    Estilos

--- app/views.py
+++ app/views.py
@@ -12,7 +12,6 @@ from keras.models import load_model
 from PIL import Image
 import numpy as np
 
-# Cargar el modelo preentrenado (asegúrate de que el archivo .h5 está en la ruta correcta)
 model = load_model(os.path.join(settings.BASE_DIR, 'app/model/model.h5'))
 
 classes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y']
@@ -34,18 +33,16 @@ def predict_hand_sign(request):
         ext = format.split('/')[-1]
         image_data = ContentFile(base64.b64decode(imgstr), name=f'hand_image_{datetime.now().strftime("%Y%m%d%H%M%S")}.{ext}')
         
-         # Abre la imagen usando PIL
         image = Image.open(image_data).convert('L')  # Convierte la imagen a escala de grises
-        image = image.resize((28, 28))  # Ajusta el tamaño según lo requiera tu modelo
+        image = image.resize((28, 28))  # Ajusta el tamaño
         image = np.array(image) / 255.0  # Normaliza la imagen
-        image = np.expand_dims(image, axis=-1)  # Añade una dimensión para el canal
-        image = np.expand_dims(image, axis=0)  # Añade una dimensión para el batch
+        image = np.expand_dims(image, axis=-1)
+        image = np.expand_dims(image, axis=0)
 
         # Realiza la predicción
         prediction = model.predict(image)
         predicted_class = np.argmax(prediction, axis=1)[0]
 
-        # Mapea la clase predicha a un signo de mano (ajusta esto según tu modelo)
         predicted_sign = classes[predicted_class]
 
         return JsonResponse({'status': 'success', 'prediction': predicted_sign})
