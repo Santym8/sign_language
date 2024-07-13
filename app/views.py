3a1cf0872d9dc1dad025162a4f316d1f30cd5bbd
SANTIAGO FIERRO <santimarcelofierrod@gmail.com>
2024-07-13 18:54:22 -0500

    Dos modelos

--- app/views.py
+++ app/views.py
@@ -15,6 +15,7 @@ from .forms import ImageForm
 import io
 
 
+model_xception = load_model(os.path.join(settings.BASE_DIR, 'app/model/model_xception.h5'))
 model = load_model(os.path.join(settings.BASE_DIR, 'app/model/model.h5'))
 
 classes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y']
@@ -35,20 +36,26 @@ def predict_hand_sign(request):
         format, imgstr = image_data.split(';base64,')
         ext = format.split('/')[-1]
         image_data = ContentFile(base64.b64decode(imgstr), name=f'hand_image_{datetime.now().strftime("%Y%m%d%H%M%S")}.{ext}')
-        
+
+        # ----------------- Modelo Previo -----------------
         image = Image.open(image_data).convert('L')  # Convierte la imagen a escala de grises
         image = image.resize((28, 28))  # Ajusta el tamaño
         image = np.array(image) / 255.0  # Normaliza la imagen
-        image = np.expand_dims(image, axis=-1)
         image = np.expand_dims(image, axis=0)
-
-        # Realiza la predicción
         prediction = model.predict(image)
         predicted_class = np.argmax(prediction, axis=1)[0]
-
         predicted_sign = classes[predicted_class]
 
-        return JsonResponse({'status': 'success', 'prediction': predicted_sign})
+        # ----------------- Modelo Xception -----------------
+        image_xception = Image.open(image_data).convert('RGB')
+        image_xception = image_xception.resize((71, 71))
+        image_xception = np.array(image_xception) / 255.0  # Normaliza la imagen
+        image_xception = np.expand_dims(image_xception, axis=0)
+        prediction_xception = model_xception.predict(image_xception)
+        predicted_class_xception = np.argmax(prediction_xception, axis=1)[0]
+        predicted_sign_xception = classes[predicted_class_xception]
+
+        return JsonResponse({'status': 'success', 'prediction': predicted_sign, 'prediction_xception': predicted_sign_xception})
     except Exception as e:
         return JsonResponse({'status': 'error', 'error': str(e)})
 
@@ -60,24 +67,34 @@ def one_image(request):
     if request.method == 'POST':
         form = ImageForm(request.POST, request.FILES)
         if form.is_valid():
-            image = request.FILES['image']
-            image_data = image.read()
+            image_xception = request.FILES['image']
+            image_data = image_xception.read()
             encoded_image = base64.b64encode(image_data).decode('utf-8')
-            image_data_url = f"data:image/{image.content_type.split('/')[-1]};base64,{encoded_image}"
+            image_data_url = f"data:image/{image_xception.content_type.split('/')[-1]};base64,{encoded_image}"
 
-            image = Image.open(io.BytesIO(image_data))
+            original_image = Image.open(io.BytesIO(image_data))
 
-            image = image.convert('L')  # Convierte la imagen a escala de grises
+            # ----------------- Modelo Previo -----------------
+            image = original_image.convert('L')  # Convierte la imagen a escala de grises
             image = image.resize((28, 28))  # Ajusta el tamaño
             image = np.array(image) / 255.0  # Normaliza la imagen
-            image = np.expand_dims(image, axis=-1)
             image = np.expand_dims(image, axis=0)
-
             prediction = model.predict(image)
             predicted_class = np.argmax(prediction, axis=1)[0]
             predicted_sign = classes[predicted_class]
 
-            return render(request, 'one_image.html', {'form': form, 'image_data_url': image_data_url, 'predicted_sign': predicted_sign})
+
+            
+            # ----------------- Modelo Xception -----------------
+            image_xception = original_image.convert('RGB')
+            image_xception = image_xception.resize((71, 71))
+            image_xception = np.array(image_xception) / 255.0  # Normaliza la imagen
+            image_xception = np.expand_dims(image_xception, axis=0)
+            prediction_xception = model_xception.predict(image_xception)
+            predicted_class_xception = np.argmax(prediction_xception, axis=1)[0]
+            predicted_sign_xception = classes[predicted_class_xception]
+
+            return render(request, 'one_image.html', {'form': form, 'image_data_url': image_data_url, 'predicted_sign_xception': predicted_sign_xception, 'predicted_sign': predicted_sign})
     else:
         form = ImageForm()
     return render(request, 'one_image.html', {'form': form, 'image_data_url': image_data_url})
